062128b1a45c13d8d4e8b8ee45fa2ab9b6e73649
Daniela Nuño <13044@portalcedi.edu.mx>
2024-01-30 13:42:04 -0600

    pedir y mostrar edad

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,7 +3,15 @@ using namespace std;
 
 int main(){
 
+    int edad;
+
     cout << "hello world!" << endl;
+    cout << "Ingrese su edad" << endl;
+    cin >> edad;
+    cout << "Edad: " << edad << endl;
+    if(edad >17){
+        cout << "es mayor de edad" << endl;
+    }
 
     return 0;
 }
